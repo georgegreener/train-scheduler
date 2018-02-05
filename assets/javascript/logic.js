02bb9de7736bdb47b0efbaa66529da52ecbef15f
George Greene <georgegreene@outlook.com>
2018-02-04 20:50:18 -0500

    added moment.js functionality

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -28,10 +28,14 @@ $("#submit").on("click", function(event) {
     // console.log(newTrain.destination);
     // console.log(newTrain.firstTrainTime);
     // console.log(newTrain.frequency);
+    $("#trainNameInput").val("");
+    $("#destinationInput").val("");
+    $("#firstTrainTimeInput").val("");
+    $("#frequencyInput").val("");
 });
 
 database.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val());
+    // console.log(childSnapshot.val());
     var trainName = childSnapshot.val().trainName;
     var destination = childSnapshot.val().destination;
     var firstTrainTime = childSnapshot.val().firstTrainTime;
@@ -40,6 +44,20 @@ database.ref().on("child_added", function(childSnapshot) {
     // console.log(destination);
     // console.log(firstTrainTime);
     // console.log(frequency);
+    var firstTrainTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+    // console.log(firstTrainTimeConverted);
+    var currentTime = moment();
+    // console.log("Current Time: " + moment(currentTime).format("hh:mm"));
+    var diffTime = moment().diff(moment(firstTrainTimeConverted), "minutes");
+    // console.log("Difference In Time: " + diffTime);
+    var timeRemainder = diffTime % frequency;
+    // console.log(timeRemainder);
+    var minutesTillTrain = frequency - timeRemainder;
+    // console.log("Minutes till train: " + minutesTillTrain);
+    var nextTrain = moment().add(minutesTillTrain, "minutes");
+    // console.log("Arrival time: " + moment(nextTrain).format("hh:mm"));
     $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>"
-        + destination + "</td><td>" + frequency + "</td></tr");
+        + destination + "</td><td>" + frequency + "</td><td>"
+        + moment(nextTrain).format("hh:mm a") + "</td><td>"
+        + minutesTillTrain + "</td></tr");
 });
