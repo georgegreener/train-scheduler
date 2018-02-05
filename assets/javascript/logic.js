1ea6ec83cfa4684e49006734b12e96fc47ad4d2d
George Greene <georgegreene@outlook.com>
2018-02-04 20:58:27 -0500

    removed console.logs and minor html tweaks

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -24,10 +24,6 @@ $("#submit").on("click", function(event) {
         frequency: frequency
     };
     database.ref().push(newTrain);
-    // console.log(newTrain.trainName);
-    // console.log(newTrain.destination);
-    // console.log(newTrain.firstTrainTime);
-    // console.log(newTrain.frequency);
     $("#trainNameInput").val("");
     $("#destinationInput").val("");
     $("#firstTrainTimeInput").val("");
@@ -35,27 +31,16 @@ $("#submit").on("click", function(event) {
 });
 
 database.ref().on("child_added", function(childSnapshot) {
-    // console.log(childSnapshot.val());
     var trainName = childSnapshot.val().trainName;
     var destination = childSnapshot.val().destination;
     var firstTrainTime = childSnapshot.val().firstTrainTime;
     var frequency = childSnapshot.val().frequency;
-    // console.log(trainName);
-    // console.log(destination);
-    // console.log(firstTrainTime);
-    // console.log(frequency);
     var firstTrainTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
-    // console.log(firstTrainTimeConverted);
     var currentTime = moment();
-    // console.log("Current Time: " + moment(currentTime).format("hh:mm"));
     var diffTime = moment().diff(moment(firstTrainTimeConverted), "minutes");
-    // console.log("Difference In Time: " + diffTime);
     var timeRemainder = diffTime % frequency;
-    // console.log(timeRemainder);
     var minutesTillTrain = frequency - timeRemainder;
-    // console.log("Minutes till train: " + minutesTillTrain);
     var nextTrain = moment().add(minutesTillTrain, "minutes");
-    // console.log("Arrival time: " + moment(nextTrain).format("hh:mm"));
     $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>"
         + destination + "</td><td>" + frequency + "</td><td>"
         + moment(nextTrain).format("hh:mm a") + "</td><td>"
